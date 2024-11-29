e3356cd8a2a658b1f26e576a0c02044f15e7ed69
S10267556 <s10267556@connect.np.edu.sg>
2024-11-29 21:36:00 +0800

    Made motto and donate button responsive

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+} /*shows the side bar*/
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none' 
+} /*hides the sidebar*/

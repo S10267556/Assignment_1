7fce166de8c15bb8e77935bad7b410dd84f23650
S10267556 <s10267556@connect.np.edu.sg>
2024-11-30 21:27:50 +0800

    Finished Our Progress

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # first
+
+https://www.geeksforgeeks.org/how-to-make-animated-counter-using-javascript/
+
+https://youtu.be/U8smiWQ8Seg?si=NRjmz6hyFKrG1z86
+
+
+images
+https://media.istockphoto.com/id/161842464/photo/oak-tree-sunset.jpg?s=612x612&w=0&k=20&c=VWJBAwdtak59CS_bCTsIOc2BbLTSJoE-CqiYfsmxrZs=
+https://images.pexels.com/photos/338936/pexels-photo-338936.jpeg?cs=srgb&dl=pexels-nejc-kosir-108379-338936.jpg&fm=jpg
+
+
+https://p7.hiclipart.com/preview/288/657/233/computer-icons-sowing-icon-design-tree-planting-agriculture-plant.jpg
+https://static.vecteezy.com/system/resources/thumbnails/002/582/996/small/donation-charity-volunteer-help-social-hands-together-community-support-line-style-icon-free-vector.jpg
+https://www.svgrepo.com/show/116369/earth-globe-tool.svg
+
+https://onetreeplanted.org/
+https://www.weforest.org/
--- script.js
+++ script.js
@@ -7,3 +7,26 @@ function hideSidebar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none' 
 } /*hides the sidebar*/
+
+let upto = 15000; /*starts counter at 15000*/
+let counts = setInterval(updated, 5000); /*counter is updated every 5 seconds*/
+
+function updated() {
+    let count = document.getElementById("tree-counter");
+    count.innerHTML = ++upto; /*increments the number*/
+    if (upto === 15825) {
+        clearInterval(counts);
+    } /*once the number reaches 15825, the counter resets to prevent lag*/
+}
+
+/*count for volunteers*/
+let uptoV = 3582; /*starts counter at 3582*/
+let countsV = setInterval(updatedV, 30000); /*counter is updated every 30 seconds*/
+
+function updatedV() {
+    let count = document.getElementById("volunteer-num");
+    count.innerHTML = ++uptoV; /*increments the number*/
+    if (uptoV === 3652) {
+        clearInterval(countsV);
+    } /*once the number reaches 3652, the counter resets to prevent lag*/
+}
